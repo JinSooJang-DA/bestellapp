35651280afafb7a6459c2f5b2ab7187acb5b22dd
JinSooJang-DA <gerkor1972@gmail.com>
2026-09-23 11:05:18 +0200

    fix : text positions changed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function renderDishes() {
-    // 1. 가져오기 및 비우기
+
     const burgerList = document.getElementById('burger-list');
     const pizzaList = document.getElementById('pizza-list');
     const saladList = document.getElementById('salad-list');
@@ -12,7 +12,6 @@ function renderDishes() {
         const menuItem = myDishes[i];
 
         if (menuItem.category === "burger") {
-            // template.js의 템플릿 함수를 호출해서 꽂아주면 코드가 훨씬 깔끔해집니다!
             burgerList.innerHTML += getMenuCardTemplate(menuItem, i);
         } else if (menuItem.category === "pizza") {
             pizzaList.innerHTML += getMenuCardTemplate(menuItem, i);
@@ -20,4 +19,8 @@ function renderDishes() {
             saladList.innerHTML += getMenuCardTemplate(menuItem, i);
         }
     }
+}
+
+function addToBasket(index) {
+    console.log('클릭한 아이템 인덱스:', index);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,21 +1,21 @@
 function getMenuCardTemplate(menuItem, i) {
     return `
     <article class="menu-card">
-            <img src="${menuItem.image}" alt="${menuItem.name}" class="menu-card-img">
-            <div class="menu-card-right">
-              <div class="menu-card-header">
-                <h4 class="menu-card-title">${menuItem.name} </h4>
-                <p class="menu-card-desc">${menuItem.description}</p>
-              </div>
-              
-              <div class="menu-card-footer">
-                <span class="menu-card-price">
-                ${menuItem.price.toFixed(2).replace('.',',')}€</span>
-                <button type="button" class="btn-add-basket"
-                onclick="addToBasket(${i})">Add to basket
-                </button>
-              </div>
+        <img src="${menuItem.image}" alt="${menuItem.name}" class="menu-card-img">
+        <div class="menu-card-right">
+            <div class="menu-card-header">
+                <h4 class="menu-card-title">${menuItem.name}</h4>
+                <span class="menu-card-price">${menuItem.price.toFixed(2).replace('.', ',')}€</span>
             </div>
+            
+            <p class="menu-card-desc">${menuItem.description}</p>
+            
+            <div class="menu-card-footer">
+                <button type="button" class="btn-add-basket" onclick="addToBasket(${i})">
+                    Add to basket
+                </button>
+            </div>
+        </div>
     </article>
-            `;
+    `;
 }
